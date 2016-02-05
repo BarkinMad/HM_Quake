32a613cdb35acbd0a8a79f65ea306953771edde2
Barkin Mad <brknsoulz@gmail.com>
2016-02-04 23:41:08 -0500

    Seperated functions into external headers

---
+++ common.c
@@ -0,0 +1,42 @@
+#include "quakedef.h"
+
+int32 com_argc = 1;
+//Parsed cmd arguments
+uint8 *com_largv[MAX_NUM_ARGVS + 1];
+
+//Check if value exists in largv & return index
+int32 COM_CheckParm(uint8 *parm)
+{
+	for (int32 i = 1; i < com_argc; i++)
+	{
+		if (!Q_strcmp(parm, com_largv[i]))
+			return i;
+	}
+	return 0;
+}
+
+//parse lpCmdLine into com_largv
+void COM_ParseCmdLine(uint8 *lpCmdLine)
+{
+	com_largv[0] = "";
+	while (*lpCmdLine && (com_argc < MAX_NUM_ARGVS + 1))
+	{
+		while (*lpCmdLine && (*lpCmdLine <= 32 || *lpCmdLine > 126))
+			lpCmdLine++;
+
+		if (*lpCmdLine)
+		{
+			com_largv[com_argc] = lpCmdLine;
+			com_argc++;
+
+			while (*lpCmdLine && (*lpCmdLine > 32 && *lpCmdLine <= 126))
+				lpCmdLine++;
+
+			if (*lpCmdLine)
+			{
+				*lpCmdLine = 0;
+				lpCmdLine++;
+			}
+		}
+	}
+}
